8a5e83704efe5cd6d4009dbccf14aef010c019c7
Thomas Williams <cree83@msn.com>
2022-12-06 22:46:23 -0500

    Completed Pokedex project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import PokemonPics from "./components/PokemonPics";
 
 function App() {
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
+
+  const getAllPokemon = async () => {
+    const res = await fetch(loadMore);
+    const data = await res.json();
+
+    setLoadMore(data.next);
+
+    function createPokemonObject(result) {
+      result.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
+
+        setAllPokemon((currentList) => [...currentList, data]);
+      });
+    }
+    createPokemonObject(data.results);
+    await console.log(allPokemon);
+  };
+
+  useEffect(() => {
+    getAllPokemon();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Pokedex</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemon.map((pokemon, index) => (
+            <PokemonPics
+              id={pokemon.id}
+              image={pokemon.sprites.other.dream_world.front_default}
+              name={pokemon.name}
+              type={pokemon.types[0].type.name}
+              key={index}
+            />
+          ))}
+        </div>
+        <button className="loadMore" onClick={() => getAllPokemon()}>
+          Catch Em!
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/PokemonPics.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PokemonPics = ({ id, name, image, type }) => {
+  const style = `pic-container ${type}`;
+  return (
+    <div className={style}>
+      <div className="number">
+        <small>#0{id}</small>
+      </div>
+      <img src={image} alt={name} />
+      <div className="detail-wrapper">
+        <h3>{name}</h3>
+        <small>Type: {type}</small>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonPics;
